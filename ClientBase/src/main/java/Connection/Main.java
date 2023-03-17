7b68783cd72e2229bd0a37e0ec1112122d1af6de
Олексій Яндюк <alex48652909@gmail.com>
2023-03-17 22:54:45 +0200

    HW 6

--- ClientBase/src/main/java/Connection/Main.java
+++ ClientBase/src/main/java/Connection/Main.java
@@ -15,7 +15,17 @@ public class Main {
             c.createTableClient();
             g.createTableGood();
             o.createTableOrder();
+            Main.step2();
+            Main.step3();
+            Main.step4();
+            Main.step5();
+        }
+    }
 
+
+    public static String step1() throws SQLException {
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            ClientDaoReq c = new ClientDaoReq(conn);
             System.out.println("Make your choose" + System.lineSeparator() + "1. Add new client" + System.lineSeparator() +
                     "2. Show all clients" + System.lineSeparator() +
                     "3. Add new good" + System.lineSeparator() + "4. Show all goods" + System.lineSeparator() +
@@ -37,10 +47,7 @@ public class Main {
 
                 System.out.println("Enter city");
                 String city = sc.nextLine();
-
-
                 c.addClient(name, sName, phone, city);
-
 
                 System.out.println("Do you want enter new client? if yes enter 1" + System.lineSeparator());
                 s = sc.nextLine();
@@ -48,6 +55,16 @@ public class Main {
                     s = "2";
                 }
             }
+            return s;
+        }
+    }
+
+    public static void step2() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            ClientDaoReq c = new ClientDaoReq(conn);
+            GoodDaoReq g = new GoodDaoReq(conn);
+            String s = Main.step1();
 
             while (s.equals("2")) {
 
@@ -61,7 +78,7 @@ public class Main {
 //            int article, String name, int price
             while (s.equals("3")) {
                 System.out.println("Enter article");
-                int article = Integer.valueOf( sc.nextLine());
+                int article = Integer.valueOf(sc.nextLine());
 
                 System.out.println("Enter name");
                 String name = sc.nextLine();
@@ -69,7 +86,7 @@ public class Main {
                 System.out.println("Enter price");
                 int price = Integer.valueOf(sc.nextLine());
 
-                g.addGood( article, name, price);
+                g.addGood(article, name, price);
 
 
                 System.out.println("Do you want enter new good? if yes enter 3" + System.lineSeparator());
@@ -78,7 +95,17 @@ public class Main {
                     s = "4";
                 }
             }
+        }
+    }
 
+
+    public static void step3() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            OrderDao o = new OrderDaoReq(conn);
+            GoodDaoReq g = new GoodDaoReq(conn);
+
+            String s = Main.step1();
             while (s.equals("4")) {
 
                 List<Good> list = g.getAllGoods();
@@ -87,15 +114,23 @@ public class Main {
                 }
                 break;
             }
+        }
+    }
 
-//            int id, String nameGood, int articleGood, int quantity, int phoneClient, String nameClient
 
+    //            int id, String nameGood, int articleGood, int quantity, int phoneClient, String nameClient
+    public static void step4() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            OrderDao o = new OrderDaoReq(conn);
+
+            String s = Main.step1();
             while (s.equals("5")) {
                 System.out.println("Enter name of good");
                 String nameGood = sc.nextLine();
 
                 System.out.println("Enter article of good");
-                int articleGood = Integer.valueOf( sc.nextLine());
+                int articleGood = Integer.valueOf(sc.nextLine());
 
                 System.out.println("Enter quantity");
                 int quantity = Integer.valueOf(sc.nextLine());
@@ -106,7 +141,7 @@ public class Main {
                 System.out.println("Enter name of client");
                 String nameClient = sc.nextLine();
 
-                o.addOrder( nameGood,  articleGood, quantity, phoneClient, nameClient);
+                o.addOrder(nameGood, articleGood, quantity, phoneClient, nameClient);
 
 
                 System.out.println("Do you want enter new order? if yes enter 5" + System.lineSeparator());
@@ -115,6 +150,15 @@ public class Main {
                     s = "6";
                 }
             }
+        }
+    }
+
+    public static void step5() throws SQLException {
+        Scanner sc = new Scanner(System.in);
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            OrderDao o = new OrderDaoReq(conn);
+
+            String s = Main.step1();
 
             while (s.equals("6")) {
 
@@ -124,11 +168,6 @@ public class Main {
                 }
                 break;
             }
-
-
-
-
         }
     }
-
 }
--- Floor/src/main/java/Connection/Main.java
+++ Floor/src/main/java/Connection/Main.java
@@ -8,17 +8,18 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * Hello world!
- */
+
 public class Main {
     public static void main(String[] args) throws SQLException {
+
+        choose2();
+    }
+    public static String choose1() throws SQLException {
         try (Connection conn = ConnectionFactory.getConnection()) {
             ClientDaoreq c = new ClientDaoreq(conn);
             c.createTable();
             System.out.println("Make your choose" + System.lineSeparator() + "1. Add new flat" + System.lineSeparator() +
                     "2. Show all flats" + System.lineSeparator() + "Enter your answer" + System.lineSeparator());
-
             Scanner sc = new Scanner(System.in);
             String s = sc.nextLine();
 
@@ -28,7 +29,6 @@ public class Main {
 
                 System.out.println("Enter square");
                 int square = Integer.valueOf(sc.nextLine());
-
 
                 System.out.println("Enter adress");
                 String adress = sc.nextLine();
@@ -40,14 +40,20 @@ public class Main {
                 int price = Integer.valueOf(sc.nextLine());
 
                 c.addFlats(region, 1, adress, rooms, price);
-
-
-                System.out.println("Do you want enter new client? if yes enter 1"+System.lineSeparator());
+                System.out.println("Do you want enter new client? if yes enter 1" + System.lineSeparator());
                 s = sc.nextLine();
                 if (!(s.equals("1"))) {
                     s = "2";
                 }
             }
+            return s;
+        }
+    }
+
+    public static void choose2() throws SQLException {
+        try (Connection conn = ConnectionFactory.getConnection()) {
+            ClientDaoreq c = new ClientDaoreq(conn);
+            String s = Main.choose1();
 
             while (s.equals("2")) {
 
@@ -61,6 +67,5 @@ public class Main {
 
         }
     }
+
 }
-
-
